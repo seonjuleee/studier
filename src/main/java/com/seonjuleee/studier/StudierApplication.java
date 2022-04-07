ebf84b4884ade31d17f03359ad706c9c31579472
seonjuleee <nlsj0820@naver.com>
2022-04-08 06:24:17 +0900

    init

---
+++ src/main/java/com/seonjuleee/studier/StudierApplication.java
@@ -0,0 +1,13 @@
+package com.seonjuleee.studier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seonjuleee/studier/StudierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seonjuleee.studier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
